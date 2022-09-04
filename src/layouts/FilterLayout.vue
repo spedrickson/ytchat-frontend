42f653fe02c08985c450509736b1ad320134f7b0
spedrickson <spedrickson@gmail.com>
2022-09-03 18:54:15 -0700

    commit in progress filtered layout

--- src/layouts/FilterLayout.vue
+++ src/layouts/FilterLayout.vue
@@ -10,8 +10,8 @@
         </q-btn-dropdown>
       </q-toolbar>
     </q-header>
-    <filtered-history :filters="filters" ref="history" @auth-error="authError" style="min-height: 400px; padding-top: 100px"/>
-    <q-drawer elevated v-model="drawerLeft" side="left" :persistent="false" class="column q-pa-sm">
+    <filtered-history class="full-width" :filters="filters" ref="history" @auth-error="authError" style="min-height: 400px"/>
+    <q-drawer v-model="drawerLeft" side="left" :persistent="false" class="column q-pa-sm">
       <q-expansion-item header-class="expand-header" class="col-auto" v-model="usersExpanded" dense dense-toggle label="Users"  icon="person_search">
         <q-btn no-wrap no-caps color="primary" icon="fas fa-user-plus" label="Add users to filter" style="width: 100%;">
           <q-menu anchor="top right" self="top left">
@@ -69,15 +69,15 @@ export default defineComponent({
     }
   },
   watch: {
-    messageTypesExpanded() {
-      this.updateCaptions();
-    }
+    messageTypesExpanded() {this.updateCaptions();},
+    userTypes() {this.modified()}
   },
   setup() {
     const urlKeyMap = {
-      "author.isChatSponsor": "isSponsor",
-      "author.isChatModerator": "isModerator",
-      "author.isVerified": "isVerified",
+      "author.channelId": "cid",
+      "author.isChatSponsor": "sponsor",
+      "author.isChatModerator": "mod",
+      "author.isVerified": "verified",
     }
     function renameQueryFilters(obj) {
       const keyValues = Object.keys(obj).map(key => {
@@ -128,6 +128,49 @@ export default defineComponent({
     }
   },
   methods: {
+    setUrlFromFilters() {
+      console.log("getting URL from filters")
+      const filters = this.getFilters().filters
+      delete filters['$or']
+      if (this.userTypes.length) {
+        console.log('manually adding user type')
+        this.userTypes.forEach(e => filters[e] = true)
+      }
+      const queryFilters = this.renameQueryFilters(filters)
+      if (this.users.size) {
+        console.log('mannually adding users')
+        queryFilters['cid'] = Array.from(this.users.keys())
+      }
+      if (this.userFilterType !== "include")  queryFilters['ufType'] = 'ex'
+      if (this.userTypeFilterType !== "include")  queryFilters['utfType'] = 'ex'
+
+      console.log(queryFilters)
+      this.$router.push({ query: queryFilters })
+      this.updateFilters() // TODO: remove
+    },
+    setFiltersFromUrl() {
+      console.log("getting filters from url")
+      const query = this.$route.query
+      if (query.types) {
+        this.messageTypes.splice(0, this.messageTypes.length)
+        if (Array.isArray(query.types)) query.types.forEach(n => this.messageTypes.push(n))
+        else this.messageTypes.push(query.types)
+      }
+
+      if (query.cid) {
+        this.users.clear()
+        if (Array.isArray(query.cid)) query.cid.forEach(u => this.users.set(u, {name: "no name, user from URL", channelId: u}))
+        else this.users.set(query.cid, {name: "no name, user from URL", channelId: query.cid})
+      }
+      else this.messageOptions.forEach(n => this.messageTypes.push(n.value))
+      if (query.sponsor != null) this.isSponsor = query.sponsor.toLowerCase() === "true" ? "true" : "false";
+      if (query.verified != null) this.isVerified = query.verified.toLowerCase() === "true" ? "true" : "false";
+
+      if (query.utfType?.startsWith('ex')) this.userTypeFilterType = 'exclude'
+      if (query.ufType?.startsWith('ex')) this.userFilterType = 'exclude'
+
+      nextTick(this.updateFilters)
+    },
     modified() {
       // console.log('modified')
       this.filtersModified = true;
@@ -138,7 +181,6 @@ export default defineComponent({
       this.modified()
     },
     removeAuthor(channelId) {
-      console.log(`removing author: ${channelId}`)
       this.users.delete(channelId)
       this.modified()
     },
@@ -154,24 +196,14 @@ export default defineComponent({
       const beforeTimestamp = this.rangePicker?.getBeforeTimestamp();
       const afterTimestamp = this.rangePicker?.getAfterTimestamp();
       if (beforeTimestamp || afterTimestamp) {
-        result.filters.timestamp = {
-          "$lte": beforeTimestamp,
-          "$gte": afterTimestamp,
-        };
+        result.filters.timestamp = {"$lte": beforeTimestamp, "$gte": afterTimestamp};
         if (!beforeTimestamp) delete result.filters.timestamp.$lte;
         if (!afterTimestamp) delete result.filters.timestamp.$gte;
       }
 
       if (this.users.size) {
-        if (this.userFilterType === "exclude") {
-          result.filters['author.channelId'] = {
-            $nin: Array.from(this.users.keys())
-          }
-        } else {
-          result.filters['author.channelId'] = {
-            $in: Array.from(this.users.keys())
-          }
-        }
+        if (this.userFilterType === "exclude") result.filters['author.channelId'] = {$nin: Array.from(this.users.keys())}
+        else result.filters['author.channelId'] = {$in: Array.from(this.users.keys())}
       }
 
       if (this.userTypes.length) {
@@ -185,7 +217,7 @@ export default defineComponent({
         })
       }
 
-      if (this.messageTypes.length !== this.messageOptions.length) {
+      if (this.messageTypes.length && this.messageTypes.length !== this.messageOptions.length) {
         result.filters.type = {"$in": this.messageTypes};
       }
       return result
@@ -194,39 +226,12 @@ export default defineComponent({
       this.filtersModified = false;
       console.log("updating filters");
       this.filters = this.getFilters();
+      console.log(this.filters)
       this.$refs?.history?.newFilters(this.filters);
       this.updateCaptions();
     },
 
-    setFiltersFromUrl() {
-      console.log("getting filters from url")
-      const query = this.$route.query
-      if (query.types) {
-        this.messageTypes.splice(0, this.messageTypes.length)
-        if (Array.isArray(query.types)) query.types.forEach(n => this.messageTypes.push(n))
-        else this.messageTypes.push(query.types)
-      }
-      else this.messageOptions.forEach(n => this.messageTypes.push(n.value))
-      if (query.isSponsor != null) this.isSponsor = query.isSponsor.toLowerCase() === "true" ? "true" : "false";
-      if (query.isVerified != null) this.isVerified = query.isVerified.toLowerCase() === "true" ? "true" : "false";
 
-      nextTick(this.updateFilters)
-    },
-
-    setUrlFromFilters() {
-      console.log("getting URL from filters")
-      const filters = this.getFilters().filters
-      delete filters['$or']
-      if (this.userTypes.length) {
-        console.log('manually adding user type')
-        this.userTypes.forEach(e => filters[e] = true)
-      }
-      const queryFilters = this.renameQueryFilters(filters)
-
-      console.log(queryFilters)
-      this.$router.push({ query: queryFilters })
-      this.updateFilters() // TODO: remove
-    }
   },
 })
 </script>
